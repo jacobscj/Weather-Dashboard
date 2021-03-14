c0639a70002eae8c4700bc0e4b1ce3e83fc4fdb7
Connor Jacobs <connorjacobs222@gmail.com>
2021-03-14 18:06:53 -0400

    establishing variables to get elements from HTML

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,15 @@
+// API Key provided by opeanweather API
 var apiKey = "a0aabd4ead254fa3fc01aaf6c28e63eb";
 
-console.log("hello");
+// establishing variables to get elements from HTML
+var citySearchEl = document.getElementById("citySearch");
+var searchEl = document.getElementById("searchButton");
+var clearHistoryEl = document.getElementById("clearHistory");
+var displayHistoryEl = document.getElementById("history");
+var cityNameEl = document.getElementById("cityName");
+var pictureEl = document.getElementById("picture");
+var temperatureEl = document.getElementById("temperature");
+var temperatureEl = document.getElementById("humidity");
+var windSpeedEl = document.getElementById("windSpeed");
+var uvIndexEl = document.getElementById("uvIndex");
+console.log("establish variables");
