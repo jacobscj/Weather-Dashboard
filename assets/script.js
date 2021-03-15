be243a468dc85c8fc722ddc4c3ab5b65d4cf4a5b
Connor Jacobs <connorjacobs222@gmail.com>
2021-03-15 15:13:20 -0400

    adding function to get UV Index

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
+$(document).ready(function(){
+
 // API Key provided by opeanweather API
 var apiKey = "a0aabd4ead254fa3fc01aaf6c28e63eb";
 
-// establishing variables to get elements from HTML
+// establishing variables to get elements from DOM
 var citySearchEl = $("#citySearch");
 var searchEl = $("#searchButton");
 var clearHistoryEl = $("#clearHistory");
@@ -14,15 +16,35 @@ var windSpeedEl = $("#windSpeed");
 var uvIndexEl = $("#uvIndex");
 var city = "";
 
+$("#searchButton").on("click", function (){
+  var searchedCity = citySearchEl.val();
+  getCurrentWeather(searchedCity)
+})
+
 function getCurrentWeather(city) {
-  var getUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "raleigh" + "&appid=" + apiKey;
+  var getUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
   $.ajax({
     url:getUrl, 
     method: "GET",
   }).then(function(response) {
     console.log(response);
+    cityNameEl.append(response.name)
+    temperatureEl.append(response.main.temp + " °F")
+    uvIndexEl.append(getUvIndex(response.coord.lat, response.coord.lon))
   });
 }
 
+function getUvIndex(lat, lon){
+  var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+  $.ajax({
+    url:uvUrl,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response.value)
+  })
+}
+
 getCurrentWeather();
+})
+
 
