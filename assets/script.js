ed920d149774377ed15a3f6c474bd1add881a925
Connor Jacobs <connorjacobs222@gmail.com>
2021-03-14 21:44:53 -0400

    testing API

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,27 @@
 var apiKey = "a0aabd4ead254fa3fc01aaf6c28e63eb";
 
 // establishing variables to get elements from HTML
-var citySearchEl = document.getElementById("citySearch");
-var searchEl = document.getElementById("searchButton");
-var clearHistoryEl = document.getElementById("clearHistory");
-var displayHistoryEl = document.getElementById("history");
-var cityNameEl = document.getElementById("cityName");
-var pictureEl = document.getElementById("picture");
-var temperatureEl = document.getElementById("temperature");
-var temperatureEl = document.getElementById("humidity");
-var windSpeedEl = document.getElementById("windSpeed");
-var uvIndexEl = document.getElementById("uvIndex");
-console.log("establish variables");
+var citySearchEl = $("#citySearch");
+var searchEl = $("#searchButton");
+var clearHistoryEl = $("#clearHistory");
+var displayHistoryEl = $("#history");
+var cityNameEl = $("#cityName");
+var pictureEl = $("#picture");
+var temperatureEl = $("#temperature");
+var humidityEl = $("#humidity");
+var windSpeedEl = $("#windSpeed");
+var uvIndexEl = $("#uvIndex");
+var city = "";
+
+function getCurrentWeather(city) {
+  var getUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "raleigh" + "&appid=" + apiKey;
+  $.ajax({
+    url:getUrl, 
+    method: "GET",
+  }).then(function(response) {
+    console.log(response);
+  });
+}
+
+getCurrentWeather();
+
